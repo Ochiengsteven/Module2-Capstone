54c679272de44f117851ea0cc1033d966ee7febd
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-01 15:36:00 +0300

    ADD COMMENTS TO RENDERLAYOUT.JS

--- src/modules/fetchApi.js
+++ src/modules/fetchApi.js
@@ -22,5 +22,3 @@ export async function getCharacterImagesAndIds() {
   }));
   return characterInfo;
 }
-
-getCharacterImagesAndIds();
--- src/modules/renderLayout.js
+++ src/modules/renderLayout.js
@@ -4,19 +4,23 @@ import logo from '../assets/logo1.png';
 
 getCharacterImagesAndIds();
 export default function renderLayout() {
+  // Create header with logo
   const logoImg = new Image();
   logoImg.src = logo;
   logoImg.alt = 'Rick and Morty logo';
   const logoDiv = document.querySelector('.logo');
   logoDiv.appendChild(logoImg);
 
+  // Create the basic layout of each character card
   const createCharacterCard = (character) => {
+    // Create the like image
     const likeImg = new Image();
     likeImg.src = like;
 
     const card = document.createElement('div');
     card.classList.add('card');
 
+    // Create the card image
     const cardImage = document.createElement('img');
     cardImage.classList.add('card-image');
     cardImage.src = character.image;
@@ -31,7 +35,7 @@ export default function renderLayout() {
     const name = document.createElement('h2');
     name.textContent = character.name;
     cardTitle.appendChild(name);
-    cardTitle.appendChild(likeImg);
+    cardTitle.appendChild(likeImg); // Append the like image to the card title
     cardContent.appendChild(cardTitle);
 
     const cardDescription = document.createElement('div');
@@ -58,9 +62,10 @@ export default function renderLayout() {
   const appendCharacterCards = async () => {
     const characters = await getCharacterImagesAndIds();
     const characterContainer = document.querySelector('.character-container');
+    // create the loop to render the cards
     characters.forEach((character) => {
-      const characterCard = createCharacterCard(character);
-      characterContainer.appendChild(characterCard);
+      const characterCard = createCharacterCard(character); // add the earlier created card layout.
+      characterContainer.appendChild(characterCard); // append the card to the container.
     });
   };
 
