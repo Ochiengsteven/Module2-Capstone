0538b39b07129ff47d124234de9638f8846a0e0b
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-01 12:12:47 +0300

    EDITED  IMPORT CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 // eslint-disable-next-line no-unused-vars
 import { fetchCharacterData, getCharacterImagesAndIds } from './modules/fetchApi.js';
-// eslint-disable-next-line no-unused-vars
-const css = require('./style.css');
+import './style.css';
