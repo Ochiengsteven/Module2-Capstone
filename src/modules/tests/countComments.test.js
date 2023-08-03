5f8192cac2035da96c811287d2d9709750c1815a
RicardoEgas <rj_egas@hotmail.com>
2023-08-03 22:14:41 +0200

    Fixed ESLint errors

--- src/modules/tests/countComments.test.js
+++ src/modules/tests/countComments.test.js
@@ -1,10 +1,10 @@
 /**
  * @jest-environment jsdom
  */
-import countComments from "../countComments";
+import countComments from '../countComments.js';
 
-describe("count comments for a character", () => {
-  test("number of comments should be 2", async () => {
+describe('count comments for a character', () => {
+  test('number of comments should be 2', async () => {
     document.body.innerHTML = `
         <div class="comments">
           <div class="comment-text"></div>
@@ -12,13 +12,13 @@ describe("count comments for a character", () => {
         </div>`;
     expect(countComments()).toBe(2);
   });
-  test("number of comments should be 0", async () => {
+  test('number of comments should be 0', async () => {
     document.body.innerHTML = `
         <div class="comments">
         </div>`;
     expect(countComments()).toBe(0);
   });
-  test("number of comments should be 5", async () => {
+  test('number of comments should be 5', async () => {
     document.body.innerHTML = `
         <div class="comments">
           <div class="comment-text"></div>
