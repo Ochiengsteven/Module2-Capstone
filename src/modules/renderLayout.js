cae1b30ebeaf8d4256fb43d52fbf1a0e9aa70da8
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-03 10:35:45 +0300

    FIX RELOAD LIKE COUNTER BUG

--- src/modules/renderLayout.js
+++ src/modules/renderLayout.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
-import { getCharacterImagesAndIds } from "./fetchApi.js";
-import logo from "../assets/logo1.png";
-import { fetchLike, postLike } from "./likesCounter.js";
-import { updateComment, addComment } from "./showComments.js";
+import { getCharacterImagesAndIds } from './fetchApi.js';
+import logo from '../assets/logo1.png';
+import { fetchLike, postLike } from './likesCounter.js';
+import { updateComment, addComment } from './showComments.js';
 
 getCharacterImagesAndIds();
 const getLikes = await fetchLike();
@@ -12,73 +12,69 @@ export default function renderLayout() {
   // Create header with logo
   const logoImg = new Image();
   logoImg.src = logo;
-  logoImg.alt = "Rick and Morty logo";
-  const logoDiv = document.querySelector(".logo");
+  logoImg.alt = 'Rick and Morty logo';
+  const logoDiv = document.querySelector('.logo');
   logoDiv.appendChild(logoImg);
 
   // Create the basic layout of each character card
   const createCharacterCard = (character) => {
-    // character.likes = 0;
-
     const likes = getLikes.map((item) => item.likes);
 
-    // Create the like image
-    const likeImg = document.createElement("h2");
-    likeImg.classList.add("likes");
+    // Create the like icon
+    const likeImg = document.createElement('h2');
+    likeImg.classList.add('likes');
     likeImg.innerHTML = '<i class="fa fa-heart-o"></i>';
 
-    const likesCount = document.createElement("p");
+    const likesCount = document.createElement('p');
     likesCount.textContent = `${likes[character.id - 1]} likes`;
 
-    likeImg.addEventListener("click", async () => {
+    likeImg.addEventListener('click', async () => {
       try {
-        // Remove the event listener to prevent further clicks
-        likeImg.classList.toggle("liked");
-        // Call the postLike function to add a like for the character
+        likeImg.classList.toggle('liked');
 
         await postLike(character.id - 1);
 
         likesCount.textContent = `${likes[character.id - 1] + 1} likes`; // Update the likes count element
       } catch (error) {
         // eslint-disable-next-line no-console
-        console.error("Error adding like:", error);
+        console.error('Error adding like:', error);
       }
     });
 
-    const card = document.createElement("div");
-    card.classList.add("card");
+    const card = document.createElement('div');
+    card.classList.add('card');
 
     // Create the card image
-    const cardImage = document.createElement("img");
-    cardImage.classList.add("card-image");
+    const cardImage = document.createElement('img');
+    cardImage.classList.add('card-image');
     cardImage.src = character.image;
     card.appendChild(cardImage);
 
-    const cardContent = document.createElement("div");
-    cardContent.classList.add("card-content");
+    const cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
     card.appendChild(cardContent);
 
-    const cardTitle = document.createElement("div");
-    cardTitle.classList.add("card-title");
-    const name = document.createElement("h2");
+    const cardTitle = document.createElement('div');
+    cardTitle.classList.add('card-title');
+    const name = document.createElement('h2');
     name.textContent = character.name;
     cardTitle.appendChild(name);
     cardTitle.appendChild(likeImg); // Append the like image to the card title
     cardContent.appendChild(cardTitle);
 
-    const cardDescription = document.createElement("div");
-    cardDescription.classList.add("card-description");
-    const countDiv = document.createElement("div");
-    countDiv.classList.add("count-div");
+    const cardDescription = document.createElement('div');
+    cardDescription.classList.add('card-description');
+    const countDiv = document.createElement('div');
+    countDiv.classList.add('count-div');
     countDiv.appendChild(likesCount);
     cardDescription.appendChild(countDiv);
-    const commentButton = document.createElement("button");
-    commentButton.classList.add("comment-button");
-    commentButton.textContent = "Comments";
+    const commentButton = document.createElement('button');
+    commentButton.classList.add('comment-button');
+    commentButton.textContent = 'Comments';
     cardDescription.appendChild(commentButton);
-    const reservationButton = document.createElement("button");
-    reservationButton.classList.add("reservation-button");
-    reservationButton.textContent = "Reservations";
+    const reservationButton = document.createElement('button');
+    reservationButton.classList.add('reservation-button');
+    reservationButton.textContent = 'Reservations';
     cardDescription.appendChild(reservationButton);
     cardContent.appendChild(cardDescription);
 
@@ -87,7 +83,7 @@ export default function renderLayout() {
 
   const appendCharacterCards = async () => {
     const characters = await getCharacterImagesAndIds();
-    const characterContainer = document.querySelector(".character-container");
+    const characterContainer = document.querySelector('.character-container');
     // create the loop to render the cards
     characters.forEach((character) => {
       const characterCard = createCharacterCard(character); // add the earlier created card layout.
@@ -95,27 +91,27 @@ export default function renderLayout() {
     });
 
     // opening comment popup page
-    const commentBtn = document.querySelectorAll(".comment-button");
+    const commentBtn = document.querySelectorAll('.comment-button');
 
     commentBtn.forEach((e) => {
-      e.addEventListener("click", async () => {
+      e.addEventListener('click', async () => {
         // finding index
         const id = [].indexOf.call(
           e.parentNode.parentNode.parentNode.parentNode.childNodes,
-          e.parentNode.parentNode.parentNode
+          e.parentNode.parentNode.parentNode,
         );
 
-        const modal = document.getElementById("comment-modal");
-        const overlay = document.createElement("comment-overlay");
-        overlay.classList.add("popup-overlay");
+        const modal = document.getElementById('comment-modal');
+        const overlay = document.createElement('comment-overlay');
+        overlay.classList.add('popup-overlay');
         modal.parentNode.insertBefore(overlay, modal);
 
-        modal.style.display = "block";
-        overlay.style.display = "block";
-        document.body.classList.add("popup-active");
-        modal.innerHTML = "";
-        const popupContainer = document.createElement("div");
-        popupContainer.classList.add("popup-container");
+        modal.style.display = 'block';
+        overlay.style.display = 'block';
+        document.body.classList.add('popup-active');
+        modal.innerHTML = '';
+        const popupContainer = document.createElement('div');
+        popupContainer.classList.add('popup-container');
         popupContainer.innerHTML = `
   <div class="comment-container">
       <img src="${characters[id].image}" id="picture" alt="">
@@ -143,28 +139,28 @@ export default function renderLayout() {
         modal.append(popupContainer);
 
         // closing comment popup
-        const closeBtn = document.querySelector(".closeBtn");
-        closeBtn.addEventListener("click", () => {
-          modal.style.display = "none";
-          overlay.style.display = "none";
-          document.body.classList.remove("popup-active");
+        const closeBtn = document.querySelector('.closeBtn');
+        closeBtn.addEventListener('click', () => {
+          modal.style.display = 'none';
+          overlay.style.display = 'none';
+          document.body.classList.remove('popup-active');
         });
-        const formBtn = document.querySelector(".submitComment");
+        const formBtn = document.querySelector('.submitComment');
         updateComment(id);
-        formBtn.addEventListener("click", async (e) => {
+        formBtn.addEventListener('click', async (e) => {
           e.preventDefault();
-          const nameInput = document.getElementById("name").value;
-          const commentInput = document.getElementById("comments").value;
+          const nameInput = document.getElementById('name').value;
+          const commentInput = document.getElementById('comments').value;
           if (nameInput && commentInput) {
             await postComment(id, nameInput, commentInput);
-            document.getElementById("myForm").reset();
+            document.getElementById('myForm').reset();
           } else {
             formBtn.insertAdjacentHTML(
-              "afterend",
-              '<p class="error-msg">Please enter a valid title and author<p>'
+              'afterend',
+              '<p class="error-msg">Please enter a valid title and author<p>',
             );
             setTimeout(() => {
-              const errorMsg = document.querySelector(".error-msg");
+              const errorMsg = document.querySelector('.error-msg');
               errorMsg.remove();
             }, 3000);
           }
